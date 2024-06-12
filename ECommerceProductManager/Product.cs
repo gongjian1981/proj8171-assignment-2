4724705b54c2ddd3d85cf7076b83f65a0f9129a7
Jian Gong <Jgong6828@conestogac.on.ca>
2024-06-12 11:07:40 -0400

    complete Product class

---
+++ ECommerceProductManager/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceProductManager
+{
+    public class Product
+    {
+        /* attributes of the Product */
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+        /**
+         * Constructor
+         */
+        public Product(int productId, string productName, decimal price, int stock) 
+        {
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        /**
+         * increase stock of prodcut
+         */
+        public void IncreaseStock(int amount)
+        {
+            Stock += amount;
+        }
+
+        /**
+         * decreae stock of product
+         */
+        public void DecreaseStock(int amount)
+        {
+            Stock -= amount;
+        }
+    }
+}
